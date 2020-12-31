550ac1ac909f553b73401f4b4a2c5b511733246f
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2020-12-31 23:47:47 +0200

    commit
    js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-window.addEventListener('load', init)
+window.addEventListener('load', init);
 
 // Globals
-let time = 5;
+
+// Available Levels 
+const levels = {
+    easy: 5,
+    medium: 3,
+    hard: 2
+}
+
+// To change level
+const currentLevel = levels.easy;
+
+let time = currentLevel;
 let score = 0;
 let isPlaying;
 
@@ -52,12 +63,45 @@ const words = [
 
 // Initialize Game
 function init() {
+    //Show number of seconds in UI
+    seconds.innerHTML = currentLevel;
     // Load word form array
     showWord(words);
+    // Start matching on word input
+    wordInput.addEventListener('input', startMatch);
     // Call countdown every second
     setInterval(countdown, 1000);
     // Check game status
     setInterval(checkStatus, 50);
+}
+
+// Start match
+function startMatch() {
+    if(matchWords()) {
+        isPlaying = true;
+        time = currentLevel + 1;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
+
+    // if score is -1, display 0;
+    if(score === -1) {
+        scoreDisplay.innerHTML = 0;
+    } else {
+        scoreDisplay.innerHTML = score;
+    }
+}
+
+// Match currentWord to wordInput
+function matchWords() {
+    if(wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Correct!!!';
+        return true;
+    } else {
+        message.innerHTML = '';
+        return false;
+    }
 }
 
 // Pick & show random word
@@ -85,5 +129,6 @@ function countdown() {
 function checkStatus() {
     if(!isPlaying && time === 0) {
         message.innerHTML = 'Game Over!!!'
+        score = -1;
     }
 }
